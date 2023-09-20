d38092298d71abe89c7ab2cc82dbf59faff23f1f
Victoryudennaka <udennakavictory@gmail.com>
2023-09-20 09:53:54 -0700

    update

--- shell.c
+++ shell.c
@@ -1,12 +1,29 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Description: This is the main function that
+ * main - Entry point of the program
+ *
+ * Description: This program prints a pattern of asterisks to the console.
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("Hello, Betty!\n");
-	return (0);
+int rows = 5;
+int i, j;
+
+for (i = 0; i < rows; i++)
+{
+for (j = 0; j < rows - i - 1; j++)
+{
+printf(" ");
+}
+for (j = 0; j < 2 * i + 1; j++)
+{
+printf("*");
+}
+printf("\n");
+}
+
+return (0);
 }
